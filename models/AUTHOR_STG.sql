c14646266794091615d80422be254b95059376bb
marouani.bilel@gmail.com <marouani.bilel@gmail.com>
2021-05-04 14:08:30 +0000

    saved as table

---
+++ models/AUTHOR_STG.sql
@@ -0,0 +1,13 @@
+--{{config}}
+{{
+  config(
+    materialized='Table'
+  )
+}}
+
+with Dim_Author as (
+       select
+         ROW_NUMBER()OVER (order by AUTHOR) as Author_id,
+          AUTHOR aS AUTHOR_NAME
+        from best_seler_categories group by AUTHOR)
+select * from Dim_Author
